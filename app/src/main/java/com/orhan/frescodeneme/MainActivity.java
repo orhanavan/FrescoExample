03375162f9dce25f8113c02e0735fec91825fad4
orhan <o.avan.71@gmail.com>
2018-08-08 12:00:21 +0300

    circle

--- app/src/main/java/com/orhan/frescodeneme/MainActivity.java
+++ app/src/main/java/com/orhan/frescodeneme/MainActivity.java
@@ -13,8 +13,19 @@ public class MainActivity extends AppCompatActivity {
         Fresco.initialize(this);
         setContentView(R.layout.fresco_layout);
 
-        SimpleDraweeView draweeView = findViewById(R.id.my_image_view);
+        // originate from
+        // http://www.devexchanges.info/2016/02/android-loading-and-caching-images-with.html
+
         final Uri imageUri = Uri.parse("https://lh3.googleusercontent.com/-voUmhKJzNHc/VSJaPfSJ2pI/AAAAAAAABKw/-oFVzRZxI40/w140-h105-p/fresh_green_grass_bokeh-wallpaper-1024x768.jpg");
+
+        SimpleDraweeView draweeView = findViewById(R.id.simple);
         draweeView.setImageURI(imageUri);
+
+        SimpleDraweeView draweeView1 = findViewById(R.id.circle);
+        draweeView1.setImageURI(imageUri);
+
+        SimpleDraweeView draweeView2 = findViewById(R.id.rounded);
+        draweeView2.setImageURI(imageUri);
+
     }
 }
